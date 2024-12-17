93ab08261d07cb91c2126635045ca80ffba0e710
Christiaan Danzfuss <cjdanzfussdbt@gmail.com>
2024-12-17 13:52:19 +0000

    added descriptions

---
+++ tests/assert_stg_payments_amount_is_positive.sql
@@ -0,0 +1,13 @@
+with payments as (
+    select * from {{ ref('stg_stripe__payments') }}
+)
+
+select
+    orderid,
+    sum(amount) as total_amount
+from
+    payments
+group by
+    orderid
+having
+    total_amount < 0
